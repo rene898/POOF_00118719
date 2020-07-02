30fe9ca8d0015e101031b50a320cb9a52a3f57e8
René <54415136+rene898@users.noreply.github.com>
2020-07-02 14:49:36 -0600

    Add: Source Code

---
+++ SourceCode/POOParcialFinal/Connection.cs
@@ -0,0 +1,35 @@
+﻿ /* using System.Data;
+using Npgsql;
+
+namespace POOParcialFinal
+{
+    public static class Connection
+    {
+        public static string CadenaConexion = 
+            "Server=127.0.0.1;Port=5432;User Id=postgres;Password=;Database=;";
+        
+        public static DataTable realizarConsulta(string sql)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(CadenaConexion);
+            DataSet ds = new DataSet();
+            
+            conn.Open();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+            da.Fill(ds);
+            conn.Close();
+            
+            return ds.Tables[0];
+        }
+
+        public static void realizarAccion(string sql)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(CadenaConexion);
+            
+            conn.Open();
+            NpgsqlCommand nc = new NpgsqlCommand(sql, conn);
+            nc.ExecuteNonQuery();
+            conn.Close();
+        }
+        
+    }
+}*/
---
+++ SourceCode/POOParcialFinal/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace POOParcialFinal
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
